86fa7bed2764d34332cf64d2cc176a25e7dd8242
Samhub24 <santoshpatange424@gmail.com>
2019-08-26 23:25:47 +0530

    updated exceptiondemo.java class

--- SampleJavaDemo/src/exceptionhandlingdemo/ExceptionDemo2.java
+++ SampleJavaDemo/src/exceptionhandlingdemo/ExceptionDemo2.java
@@ -1,20 +1,22 @@
 package exceptionhandlingdemo;
 
+// @auther name is santosh patange
+
 public class ExceptionDemo2 {
 
 	public static void main(String[] args) {
-		
-	
+
+
 		try {
 			System.out.println("/n******Hello World*******/n");
 			int i=1/0;
 			System.out.println("completed");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			
+
 			e.printStackTrace();
 		}
-		
+
 	}
 }
 
